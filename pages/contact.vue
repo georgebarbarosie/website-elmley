db2897756ae9bc31a84af8a142db3406cdc353ed
George Barbarosie <george.barbarosie@tripsolutions.co.uk>
2025-12-04 14:27:28 +0000

    slightly more agreeable layout for the contact card

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,32 +1,12 @@
 <template>
-    <!-- <div class="flex">
-        <Carousel class="flex-1" v-bind="carouselConfig">
-            <Slide>
-                <img src="~/assets/images/contact-desk.png">
-            </Slide>
-        </Carousel>
-    </div>
-
-
-    <div class="card">
-        <p class="hyphens-auto" lang="en">
-            Email: <a href="mailto:office@elmleyhomes.co.uk">office@elmleyhomes.co.uk</a>
-        </p>
-
-        <p class="hyphens-auto" lang="en">
-            Phone: <a href="tel:+44 774599099">+44 7745 99099</a>
-        </p>
-    </div> -->
-
-    <div class="card flex flex-col md:flex-row md:items-center">
-        <div class="flex-1" style="margin: -16px;">
+    <div class="card flex flex-col md:flex-row md:items-center md:align-top">
+        <div class="flex-1 -m-4">
             <NuxtImg 
                 src="/images/contact-desk.png"
-                class="rounded-lg shadow-lg"
-                style="max-width:800px; max-height:600px; width:100%; height:auto; object-fit:cover;"
+                class="rounded-lg shadow-lg max-w-[800px] max-h-[600px] h-auto w-[100%] object-cover"
             />
         </div>
-        <div class="flex-1 mt-8 md:mt-0 lg:mt-0 md:ml-6">
+        <div class="flex-1 mt-8 md:mt-0 lg:mt-0 md:ml-6 md:h-[100%] p-8">
                 <table class="flex">
                     <tbody>
                     <tr>
@@ -50,11 +30,6 @@
 </template>
 <script setup lang="ts">
 import { NuxtImg } from '#components';
-import { Carousel, Slide, Navigation, Pagination} from 'vue3-carousel';
-
-const carouselConfig = {
-  height: '40em'
-}
 
 useHead({
     title: 'Contact Us - Elmley Homes',
